8654975af061d74c5d9348db0bcab5ecd592f520
Southrays <southrays1@gmail.com>
2026-01-18 12:11:12 +0100

    Add DSC Token

--- src/DecentralizedStablecoin.sol
+++ src/DecentralizedStablecoin.sol
@@ -24,22 +24,17 @@ contract DecentralizedStablecoin is ERC20Burnable, Ownable {
     error DecentralizedStablecoin__NotZeroAddress(); 
     error DecentralizedStablecoin__BurnAmountExceedsBalance();
 
-
-    ///////////////////////////////
-    /////     Variables     //////
-    /////////////////////////////
-
     /////////////////////////////////
     /////     Constructor     //////
     ///////////////////////////////
-    constructor() ERC20("DecentralizedToken", "DSC") Ownable(msg.sender) {}
+    constructor() ERC20("DecentralizedStablecoin", "DSC") Ownable(msg.sender) {}
 
     /**
-     * This function can only be called by the owner to burn DecentralizedToken (DSC).
+     * This function can only be called by the owner to burn DecentralizedStablecoin (DSC).
      * The owner is the DSCEngine contract.
-     * This function burns a specific amount of DecentralizedToken (DSC) from the caller's account.
+     * This function burns a specific amount of DecentralizedStablecoin (DSC) from the caller's account.
      * This function ensures that the amount to be burned is greater than zero and does not exceed the caller's balance.
-     * @param _amount The amount of DecentralizedToken (DSC) to burn
+     * @param _amount The amount of DecentralizedStablecoin (DSC) to burn
      */
     function burn(uint256 _amount) public override onlyOwner {
         uint256 balance = balanceOf(msg.sender);
@@ -50,12 +45,12 @@ contract DecentralizedStablecoin is ERC20Burnable, Ownable {
 
 
     /**
-     * This function can only be called by the owner to mint new DecentralizedToken (DSC).
+     * This function can only be called by the owner to mint new DecentralizedStablecoin (DSC).
      * The owner is the DSCEngine contract.
      * This function ensures that the recipient address is not the zero address and that the amount to be minted is greater than zero.
-     * This function mints a specific amount of DecentralizedToken (DSC) to the specified address.
+     * This function mints a specific amount of DecentralizedStablecoin (DSC) to the specified address.
      * @param _to The address to mint the tokens to.
-     * @param _amount The amount of DecentralizedToken (DSC) to mint.
+     * @param _amount The amount of DecentralizedStablecoin (DSC) to mint.
      */
     function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
         if (_to == address(0)) revert DecentralizedStablecoin__NotZeroAddress();
